333c8eb4af2ee2d28a5170e16833fefc301885fd
Cesia <cesiamendoza4817mail.com>
2026-05-02 22:05:12 -0600

    Se quito el boton de la parte de arriba

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,12 @@ function App() {
         className="navbar navbar-expand-lg navbar-dark fixed-top"
         style={{ backgroundColor: "#026102" }}
       >
-        <div className="container-fluid">
+        <div className="container-fluid justify-content-center">
           <a className="navbar-brand fw-bold" href="#">
             OXIGENIUS
           </a>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Monitoreo</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Proyecto</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contacto</a>
-              </li>
-            </ul>
           </div>
-        </div>
       </nav>
 
       {/* CONTENIDO (FUERA DEL NAV) */}
